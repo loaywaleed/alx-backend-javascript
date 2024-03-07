07e09662e1137698fac265cddd90a77965c0daa9
Loay Waleed <loaywaleed9@gmail.com>
2024-03-07 14:57:24 +0200

    Typo

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
 export default function getFullResponseFromAPI(success) {
   if (success === true) {
-    return Promise.resolve({ status: 200, body: 'success' });
+    return Promise.resolve({ status: 200, body: 'Success' });
   }
   return Promise.reject(new Error('The fake API is not working currently'));
 }
