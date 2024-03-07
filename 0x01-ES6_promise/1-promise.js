37f83dc6a445826dad076d87a8734697c11e4058
Loay Waleed <loaywaleed9@gmail.com>
2024-03-07 14:55:42 +0200

    resolving and rejecting depending on parameters

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,6 @@
+export default function getFullResponseFromAPI(success) {
+  if (success === true) {
+    return Promise.resolve({ status: 200, body: 'success' });
+  }
+  return Promise.reject(new Error('The fake API is not working currently'));
+}
