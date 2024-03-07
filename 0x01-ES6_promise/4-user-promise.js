bfceeef9d9341fd12e1b0617505b32a7cc08b075
Loay Waleed <loaywaleed9@gmail.com>
2024-03-07 22:43:29 +0200

    making it cleaner

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,3 @@
 export default function signUpUser(firstName, lastName) {
-  const obj = {
-    firstName,
-    lastName
-  };
-  return Promise.resolve(obj);
+  return Promise.resolve({ firstName, lastName });
 }
