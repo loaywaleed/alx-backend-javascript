47328a801b3e61453cfb34fd9604a779c6900617
Loay Waleed <loaywaleed9@gmail.com>
2024-03-07 22:41:08 +0200

    changing comma

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,7 @@
 export default function signUpUser(firstName, lastName) {
   const obj = {
-    firstName : firstName,
-    lastName : lastName,
-  }
+    firstName,
+    lastName
+  };
   return Promise.resolve(obj);
 }
