5cde820e6413b6bd05bfcdb827bb7f277a834dee
Loay Waleed <loaywaleed9@gmail.com>
2024-03-08 21:50:31 +0200

    destructuring and mapping

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]).then(
+    (response) => 
+    response.map(({status, value, reason}) => ({
+      status, 
+      value: (status) => {
+          if (status === 'fulfilled') {
+            return value;
+          }
+          return reason.toString();
+        },
+    })));
+}
