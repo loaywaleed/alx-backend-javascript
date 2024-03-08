40db9f099daf2a102d9a347d9fff77786ca69352
Loay Waleed <loaywaleed9@gmail.com>
2024-03-08 22:41:12 +0200

    cleanup

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,8 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]).then(
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then(
     (response) => response.map(({ status, value, reason }) => ({
       status,
       value: () => {
