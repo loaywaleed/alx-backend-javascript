bf5498ab26935763141a09a13de5e496061d98fb
Loay Waleed <loaywaleed9@gmail.com>
2024-03-09 00:43:31 +0200

    eslint

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,7 @@
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
-    throw Error('cannot divide by 0')
+    throw Error('cannot divide by 0');
   }
-  let result = numerator / denominator;
+  const result = numerator / denominator;
   return result;
 }
