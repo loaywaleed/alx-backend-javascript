63595a987dfd0eb6d05968047468262e8a85dd0c
Loay Waleed <loaywaleed9@gmail.com>
2024-03-08 21:51:06 +0200

    eslint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,14 +3,14 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]).then(
-    (response) => 
-    response.map(({status, value, reason}) => ({
-      status, 
+    (response) => response.map(({ status, value, reason }) => ({
+      status,
       value: (status) => {
-          if (status === 'fulfilled') {
-            return value;
-          }
-          return reason.toString();
-        },
-    })));
+        if (status === 'fulfilled') {
+          return value;
+        }
+        return reason.toString();
+      },
+    })),
+  );
 }
