3cdf76042760bf4250322025e7985892cc12b589
loaywaleed <loaywaleed9@gmail.com>
2024-03-13 22:23:18 +0200

    class interfaces

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,4 +16,37 @@ interface printTeacherFunction {
 
 };
 
-const printTeacher: printTeacherFunction = (firstName: , lastName) => `${firstName[0]}. ${lastName}`;
+export const printTeacher: printTeacherFunction = (firstName , lastName) => `${firstName[0]}. ${lastName}`;
+
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentClassConstructorInterface {
+  new(firstName: string, lastName: string): StudentClassInterface; 
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+      return 'Currently working';
+  }
+
+  displayName(): string {
+      return this.firstName;
+  }
+}
+
+function createStudent(cStudent: StudentClassConstructorInterface, firstName: string, lastName: string): StudentClassInterface {
+  return new cStudent(firstName, lastName);
+}
+
