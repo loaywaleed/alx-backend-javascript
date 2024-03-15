ffef79a54d0e5f3877fc71cbe79ae6c1d292b711
loaywaleed <loaywaleed9@gmail.com>
2024-03-16 01:01:38 +0200

    eslint

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 export default function getListStudends() {
-  const newArr = []
+  const newArr = [];
   const student1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
   const student2 = { id: 2, firstName: 'James', location: 'Columbia' };
   const student3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
   newArr.push(student1, student2, student3);
-  return newArr
+  return newArr;
 }
