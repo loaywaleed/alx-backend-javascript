f59ba91ee6c1026358877a009521f6830ba23c30
loaywaleed <loaywaleed9@gmail.com>
2024-03-16 03:48:04 +0200

    eslint

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,11 @@
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
-    throw new Error('Cannot process')
-  };
+    throw new Error('Cannot process');
+  }
   for (const [k, v] of map.entries()) {
     if (v === 1) {
-      map.set(k, 100)
-    };
+      map.set(k, 100);
+    }
   }
   return map;
 }
