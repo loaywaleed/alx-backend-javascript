901534d186aa3a570d185488470205832e194c69
loaywaleed <loaywaleed9@gmail.com>
2024-03-16 03:39:33 +0200

    eslint

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (!startString || !startString.length || typeof startString !== 'string') return '';
-  const newArr = [...set].filter(item => item.startsWith(startString));
-  const mapped = newArr.map(item => item.replace(startString, ''))
+  if (!startString || !startString.length || typeof startString !== 'string' || !(set instanceof Set)) {
+    return '';
+  }
+  const newArr = [...set].filter((item) => item.startsWith(startString));
+  const mapped = newArr.map((item) => item.replace(startString, ''));
   const str = mapped.join('-');
   return str;
 }
