80774a7d37b7fe4e5c1e27a2887d361055c2e664
loaywaleed <loaywaleed9@gmail.com>
2024-03-16 03:36:51 +0200

    more cases

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (set === false || startString === '') {
-    return '';
-  }
+  if (!startString || !startString.length || typeof startString !== 'string') return '';
   const newArr = [...set].filter(item => item.startsWith(startString));
   const mapped = newArr.map(item => item.replace(startString, ''))
   const str = mapped.join('-');
