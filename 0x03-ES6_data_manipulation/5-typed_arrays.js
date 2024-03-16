313ed3e5bd635eb98ad83a9d424c2037a4874525
loaywaleed <loaywaleed9@gmail.com>
2024-03-16 03:02:51 +0200

    typed arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,8 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  const typedArray = new Int8Array(length);
+  typedArray[position] = value;
+  return typedArray.buffer;
+}
