16e7eda05345e5a28b71d55c2713fdff12ef2425
loaywaleed <loaywaleed9@gmail.com>
2024-03-16 02:43:41 +0200

    hotfix

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,8 +2,8 @@ export default function updateStudentGradeByCity(lst, city, newGrades) {
   const arr = lst.filter((item) => item.location === city);
   const newArr = arr.map((item) => {
     const grades = newGrades.find((grade) => grade.studentId === item.id);
-    const newGrade = grades || 'N/A';
-    return { ...item, newGrade };
+    const grade = grades || 'N/A';
+    return { ...item, grade };
   });
   return newArr;
 }
