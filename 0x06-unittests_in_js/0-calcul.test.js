959d76ea954d92c2501f5496b4d01d718ba455c1
loaywaleed <loaywaleed9@gmail.com>
2024-05-03 05:17:49 +0300

    tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -9,4 +9,12 @@ describe('calculateNumber', () => {
   it('1 and 3.7', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
+
+  it('-1 and -3.7', () => {
+    assert.strictEqual(calculateNumber(-1, 3.7), 3);
+  });
+
+  it('-1 and -3.7', () => {
+    assert.strictEqual(calculateNumber(-1, -3.7), -5);
+  });
 });
